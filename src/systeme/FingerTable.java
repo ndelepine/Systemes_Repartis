c50a8aa40a03e2629d654fc22dc3d381baacee4d
jbqueyrie <jbqueyrie@gmail.com>
2015-12-02 16:52:02 +0100

    Premier test de la FingerTable

--- src/systeme/FingerTable.java
+++ src/systeme/FingerTable.java
@@ -6,11 +6,26 @@ import actors.ChordNode;
 
 public class FingerTable {
 	
-	TreeMap fingerTable = new TreeMap();
+	TreeMap<Integer,TableEntry> fingerTable = new TreeMap<Integer,TableEntry>();
 	
-	public TableEntry fingertableEntry (int numLine, int IDnoeud, int lowerBound, int upperBound, ChordNode referent){
+	public FingerTable(){
+		
+		for (int i=1;i<=4;i++){
+			ChordNode cntest = new ChordNode();
+			fingertableEntry(i,i+1,i+2,i+3,cntest);
+		}
+	}
+	public void fingertableEntry (int numLine, int IDnoeud, int lowerBound, int upperBound, ChordNode referent){
 		TableEntry tableEntry = new TableEntry(IDnoeud, lowerBound,upperBound, referent);
 		fingerTable.put(numLine, tableEntry);
-		return tableEntry;
 	}
+
+	public TreeMap<Integer,TableEntry> getFingerTable() {
+		return fingerTable;
+	}
+
+	public void setFingerTable(TreeMap fingerTable) {
+		this.fingerTable = fingerTable;
+	}
+	
 }
